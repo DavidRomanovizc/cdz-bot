9fe09bca580ecb80f2acdb2347115f254dd5e41d
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-18 22:58:56 +0300

    ✨ Added utils
    database, throttling etc

---
+++ utils/misc/graph.py
@@ -0,0 +1,2 @@
+import matplotlib as plt
+
---
+++ utils/notify_admins.py
@@ -0,0 +1,15 @@
+from loguru import logger
+
+from aiogram import Dispatcher
+from aiogram.utils.exceptions import ChatNotFound
+
+from data.config import ADMINS
+
+
+async def on_startup_notify(dp: Dispatcher):
+    logger.info("Оповещение администрации...")
+    for admin in ADMINS:
+        try:
+            await dp.bot.send_message(admin, "Бот был успешно запущен", disable_notification=True)
+        except ChatNotFound:
+            logger.debug("Чат с админом не найден")
