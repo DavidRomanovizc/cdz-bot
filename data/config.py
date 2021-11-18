5dad0701276fe13fc4a7f196e44ef523f32f4476
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-18 23:05:08 +0300

    ✨Added data

---
+++ data/config.py
@@ -0,0 +1,14 @@
+from environs import Env
+
+# Теперь используем вместо библиотеки python-dotenv библиотеку environs
+env = Env()
+env.read_env()
+
+BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
+ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
+IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+
+DB_USER = env.str('DB_USER')
+DB_PASS = env.str('DB_PASS')
+DB_HOST = env.str('DB_HOST')
+DB_NAME = env.str('DB_NAME')
