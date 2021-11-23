50ad12329c1698a9f67fd6e6db47451c705dcdb8
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-23 17:36:16 +0300

    ♻️ Refactor code

--- utils/misc/graph.py
+++ utils/misc/graph.py
@@ -1,2 +1,8 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
+from loader import db
+
+x = [752, 123, 8990, 456, 765, 890, 799, 114, 1421, 4839, 3124]
+y = [1, 56, 78, 99, 114, 145, 178, 193, 211, 254, 275]
+
+plt.plot(x, y)
