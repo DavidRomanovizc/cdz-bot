14c03c3103b066d0a8f0c6d8120bfbc9c8320e1a
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-11-23 17:35:59 +0300

    ♻️ Refactor code

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,10 +1,8 @@
-from aiogram import types
 from aiogram.dispatcher import FSMContext
-
+from aiogram import types
 from loader import dp
 
 
-# Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
     await message.answer(f"Эхо без состояния."
@@ -12,7 +10,6 @@ async def bot_echo(message: types.Message):
                          f"{message.text}")
 
 
-# Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
 @dp.message_handler(state="*", content_types=types.ContentTypes.ANY)
 async def bot_echo_all(message: types.Message, state: FSMContext):
     state = await state.get_state()
