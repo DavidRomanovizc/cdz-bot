38efcdba348f3d7a67562a2c9be8e55996f8b175
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-10 12:53:26 +0300

    Update

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -11,12 +11,14 @@ async def bot_start(message: types.Message):
         await db.add_user_Users(full_name=message.from_user.full_name,
                                 telegram_id=message.from_user.id,
                                 username=message.from_user.username)
-        await message.reply(text=(f"Приветствуем вас, уважаемый/уважаемая {message.from_user.full_name}!\n\n"
-                                  f"Это система для решения онлайн тестов на образовательных платформах РФ\n\n"
-                                  f"Бот не может решать тесты, где есть задания ***открытого типа***\n"),
-                            reply_markup=main_menu)
     except UniqueViolationError:
+        user = await db.select_user(telegram_id=message.from_user.id)
+        is_premium = user.get('is_premium')
+        balance = user.get('balance')
         await message.reply(text=(f"Приветствуем вас, уважаемый/уважаемая {message.from_user.full_name}!\n\n"
+                                  f"💸 Ваш баланс: ***{balance}*** | Подписка: ***{is_premium}***\n | True - уже "
+                                  f"куплена | "
+                                  f"False - не куплена|\n\n "
                                   f"Это система для решения онлайн тестов на образовательных платформах РФ\n\n"
                                   f"Бот не может решать тесты, где есть задания ***открытого типа***\n"),
                             reply_markup=main_menu)
