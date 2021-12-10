89dab41ec5658bfb653c8563377f7712d461e166
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-10 12:56:13 +0300

    Commit

--- keyboards/inline/btn_menu.py
+++ keyboards/inline/btn_menu.py
@@ -4,12 +4,17 @@ main_menu = InlineKeyboardMarkup(row_width=4,
                                  inline_keyboard=[
                                      [
                                          InlineKeyboardButton(text="🤖 О боте", callback_data="about_bot"),
-                                         InlineKeyboardButton(text="📰 Информация", callback_data="information")
+                                         InlineKeyboardButton(text="📗 Руководство", callback_data="guide_btn")
+                                     ],
+                                     [
+
+                                         InlineKeyboardButton(text="📈 Статистика", callback_data="stats")
+
                                      ],
                                      [
                                          InlineKeyboardButton(text="💻 ‍Решить тест", callback_data="pass_test")
                                      ],
                                      [
-                                         InlineKeyboardButton(text="📈 Статистика", callback_data="stats")
+                                         InlineKeyboardButton(text="💸 Приобрести подписку", callback_data="pay_balance")
                                      ]
                                  ])
