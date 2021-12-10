6195b2d99a990a8073548cffb8b38d99cd363f56
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-10 22:46:14 +0300

    fix bugs

--- handlers/users/about_bot.py
+++ handlers/users/about_bot.py
@@ -5,7 +5,6 @@ from loader import dp
 
 @dp.callback_query_handler(text="about_bot")
 async def get_inf_bot(call: CallbackQuery):
-    await call.answer(cache_time=60)
     await call.message.edit_text(f"***👑 О боте***\n"
                                  f"Этот бот решает цдз тесты\n"
                                  f"Creator ***@DRomanovizc***", reply_markup=supported)
