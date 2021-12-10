19022f2c4e188fa525801efb5c5c1bf5a32664db
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-10 12:56:19 +0300

    Commit

--- handlers/users/back_handler.py
+++ handlers/users/back_handler.py
@@ -1,9 +1,21 @@
 from keyboards.inline.btn_menu import main_menu
 from aiogram.types import CallbackQuery
-from loader import dp
+from loader import dp, bot
 
 
 @dp.callback_query_handler(text="back")
 async def back_to_menu(call: CallbackQuery):
     await call.answer(cache_time=60)
-    await call.message.edit_text("Вы вернулись в меню: ", reply_markup=main_menu)
+    await call.message.edit_text("***👨‍💻 Cdz.Bot*** - телеграм бот, который может решать цдз тесты\n\n"
+                                 "***🤝 Сотрудничество:***\n"
+                                 "Если у вас есть предложение о сотрудничестве, пишите сюда - "
+                                 "@DRomanovizc", reply_markup=main_menu)
+
+
+@dp.callback_query_handler(text="close_everything")
+async def get_close_everything(call: CallbackQuery):
+    await bot.delete_message(chat_id=call.from_user.id, message_id=call.message.message_id)
+    await call.message.answer("***👨‍💻 Cdz.Bot*** - телеграм бот, который может решать цдз тесты\n\n"
+                              "***🤝 Сотрудничество:***\n"
+                              "Если у вас есть предложение о сотрудничестве, пишите сюда - "
+                              "@DRomanovizc", reply_markup=main_menu)
