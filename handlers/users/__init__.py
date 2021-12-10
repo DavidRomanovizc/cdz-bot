392417a99a2995fbb468f58264c39b40f927d33b
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-10 12:56:24 +0300

    Commit

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,6 +2,7 @@ from . import help
 from . import start
 from . import about_bot
 from . import back_handler
-from . import info_handler
+from . import guide_handler
 from . import statistics
+from . import qiwi
 from . import test
